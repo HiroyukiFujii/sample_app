6524e14ac1450c9c6a290531869ef909c5423d86
HiroyukiFujii <h.fujii1102@gmail.com>
2016-12-25 13:12:35 +0900

    Implement basic login function

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+  end
+  
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do #User.countに変化がないことの確認
@@ -13,6 +17,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 
   test "valid signup information" do
